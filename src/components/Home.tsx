04344281e29b37506cec9e87d047e1cc62c08473
ChelsiGarg <chelsigarg178@gmail.com>
2026-01-31 17:44:07 +0530

    Created multiple team cards on home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,25 @@
 // Components
 import TeamCard from "./TeamCard";
 
+// Mock Data and its types
+import teamsData from "../mock/db.json";
+import type { Team } from "../types/team";
+
+const teams = teamsData as Team[];
+
 const Home = () => {
   return (
-    <TeamCard />
+    <>
+      {
+        teams.map(team => (
+          <TeamCard 
+            logo = {team.logo}
+            name = {team.name}
+            summary= {team.summary}
+          />
+        ))
+      }
+    </>
   )
 }
 
---
+++ src/types/team.ts
@@ -0,0 +1,47 @@
+export interface Team {
+    id: string;
+    name: string;
+    logo: string;
+    summary: string;
+    overview: Overview;
+    members: Member[];
+    projects: Project[];
+    techStack: TechStack;
+    responsibilities: Responsibilities;
+}
+
+export interface Overview {
+    description: string;
+    mission: string;
+    orgFit: string;
+    stakeholders?: string[];
+}
+
+export interface Member {
+    id: string;
+    name: string;
+    role: string;
+    experience: string;
+    specialization: string;
+    email: string;
+}
+
+export interface Project {
+    id: string;
+    name: string;
+    status: "Active" | "Completed";
+    techUsed: string[];
+    description?: string;
+}
+
+export interface TechStack {
+    languages: string[];
+    frameworks: string[];
+    tools: string[];
+    platforms: string[];
+}
+
+export interface responsibilities {
+    owns: string[];
+    doesNotOwn: string[];
+}
