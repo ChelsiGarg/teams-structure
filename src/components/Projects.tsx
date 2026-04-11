3e4923b262b73648e0309f4aab080cbf8647b075
Garg <chelsi_garg@optum.com>
2026-04-11 18:40:56 +0530

    create status dropdown

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,18 @@
+// React libraries
+import { useState } from "react";
+
 // MUI libraries
 import { MenuItem, Stack, TextField } from "@mui/material"
 
+
 const Projects = () => {
+  const [statuses, setStatuses] = useState<string[]>([]);
+
+  const handleStatusChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setStatuses(typeof value === 'string' ? value.split(',') : value);
+  }
+
   return (
     <Stack sx={{ p: 2, mt: 0.7 }} spacing={4}>
 
@@ -10,6 +21,13 @@ const Projects = () => {
         <TextField 
           label="Status" 
           select 
+          value={statuses}
+          onChange={handleStatusChange}
+          slotProps={{
+            select: {
+              multiple: true
+            }
+          }}
         >
           <MenuItem value="active">Active</MenuItem>
           <MenuItem value="inactive">Inactive</MenuItem>
