1b1ffbeb87dd28082845ce67c5cc148d418d1402
ChelsiGarg <chelsigarg178@gmail.com>
2026-04-11 18:26:48 +0530

    Start building Projects UI

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,23 @@
+// MUI libraries
+import { MenuItem, Stack, TextField } from "@mui/material"
 
 const Projects = () => {
   return (
-    <div>
-      Projects
-    </div>
+    <Stack sx={{ p: 2, mt: 0.7 }} spacing={4}>
+
+      {/* ask copilot to give suitable comment here about the below section */}
+      <Stack direction="row" display="flex">
+        <TextField 
+          label="Status" 
+          select 
+        >
+          <MenuItem value="active">Active</MenuItem>
+          <MenuItem value="inactive">Inactive</MenuItem>
+          <MenuItem value="completed">Completed</MenuItem>
+        </TextField>
+      </Stack>
+
+    </Stack>
   )
 }
 
